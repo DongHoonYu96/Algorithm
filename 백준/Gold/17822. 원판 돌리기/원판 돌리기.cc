bab6a8b916a90c196e4f3d93ad2eef1c28967871
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-03-04 21:26:49 +0900

    [Gold II] Title: 원판 돌리기, Time: 4 ms, Memory: 2248 KB -BaekjoonHub

--- 백준/Gold/17822. 원판 돌리기/원판 돌리기.cc
+++ 백준/Gold/17822. 원판 돌리기/원판 돌리기.cc
@@ -8,22 +8,28 @@ const int dy[] = {-1, 0, 1, 0};  // 상, 우, 하, 좌 (y 방향)
 const int dx[] = {0, 1, 0, -1};
 
 void calc() {
-   ret = 0;
    for(int i=0;i<n;++i) {
       for(int j=0;j<m;++j) {
-         ret += v[i][j];
+         ret+=v[i][j];
       }
    }
 }
-
-void go(int idx) {
-   int mod_k = k % m;
-   if (mod_k == 0) return; // 회전할 필요 없음
-   if (d == 1) { // 반시계 방향
-      rotate(v[idx].begin(), v[idx].begin() + mod_k, v[idx].end());
-   } else { // 시계 방향
-      rotate(v[idx].begin(), v[idx].begin() + (m - mod_k), v[idx].end());
+void print() {
+   for(int i=0;i<n;++i) {
+      for(int j=0;j<m;++j) {
+         cout<<v[i][j]<<" ";
+      }cout<<"\n";
    }
+}
+// idx먼째 벡터를 회전시키는 함수
+void go(int idx) {
+   if (d == 1) {
+      rotate(v[idx].begin(), v[idx].begin() + k, v[idx].end());
+   } else {
+      rotate(v[idx].begin(), v[idx].begin() + v[idx].size() - k, v[idx].end());
+   }
+   // print();
+   
 }
 
 void calcAvg() {
@@ -47,37 +53,63 @@ void calcAvg() {
    }
 }
 
+// void calcAvg() {
+//    //평균계산
+//    int sum=0; int cnt=0;
+//    for(int i=0;i<n;++i) {
+//       for(int j=0;j<m;++j) {
+//          sum+=v[i][j];
+//          if(v[i][j]!=0) cnt++;
+//       }
+//    }
+//    double avg = (double)sum/(double)cnt;
+//
+//    // 더하기
+//    for(int i=0;i<n;++i) {
+//       for(int j=0;j<m;++j) {
+//          if(v[i][j]==0) continue;
+//          if((double)v[i][j]>avg) {
+//             v[i][j]--;
+//          }
+//          if((double)v[i][j] < avg) {
+//             v[i][j]++;
+//          }
+//       }
+//    }
+// }
+
 void dfs(int y, int x) {
    for(int i=0;i<4;++i) {
-      int ny = y + dy[i];
-      int nx = (x + dx[i] + m) % m;
+      int ny=y+dy[i];
+      int nx=(x+dx[i]+m)%m; // ???
 
-      if(ny < 0 || ny >= n) continue;
+      if(ny<0 || ny>=n) continue;
       if(vis[ny][nx]) continue;
 
-      if(v[y][x] == v[ny][nx] && v[y][x] != 0) {
-         vis[y][x] = vis[ny][nx] = 1;
-         found = 1;
-         dfs(ny, nx);
+      if(v[y][x]==v[ny][nx]) {
+         vis[y][x]=vis[ny][nx]=1;
+         found=1;
+         dfs(ny,nx);
       }
    }
 }
 
 void findAdj() {
-   memset(vis, 0, sizeof(vis));
-   found = 0;
+   found=0;
+   memset(vis,0,sizeof(vis));
 
    for(int i=0;i<n;++i) {
       for(int j=0;j<m;++j) {
-         if(v[i][j] == 0 || vis[i][j]) continue;
-         dfs(i, j);
+         if(v[i][j]==0) continue;
+         if(vis[i][j]) continue;
+         dfs(i,j);
       }
    }
 
    for(int i=0;i<n;++i) {
       for(int j=0;j<m;++j) {
          if(vis[i][j]) {
-            v[i][j] = 0;
+            v[i][j]=0;
          }
       }
    }
@@ -87,28 +119,31 @@ int main(){
    ios_base::sync_with_stdio(0);
    cin.tie(0); cout.tie(0);
 
-   cin >> n >> m >> t;
+   cin>>n>>m>>t;
    for(int i=0;i<n;++i) {
       for(int j=0;j<m;++j) {
          int tmp;
-         cin >> tmp;
+         cin>>tmp;
          v[i].push_back(tmp);
       }
    }
 
    for(int i=0;i<t;++i) {
-      cin >> x >> d >> k;
-      k %= m; // k를 m으로 나눈 나머지로 조정
+      cin>>x>>d>>k;
       for(int j=0;j<n;++j) {
-         if((j+1) % x == 0) {
+         if((j+1)%x==0) {
+            // cout<<"rotate "<<j<<"\n";
             go(j);
          }
       }
       findAdj();
+      // searchUpDown();
+      // searchLeftRight();
       if(!found) {
          calcAvg();
       }
    }
    calc();
-   cout << ret;
+   // print();
+   cout<<ret;
 }
