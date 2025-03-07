f0b6faebae0e61d91cc22d10ad8c3163f71ddb87
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-03-07 13:49:38 +0900

    [Silver I] Title: 기타 레슨, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -1,61 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
-int n, m;
+int n,m,ret;
 int a[100000+4];
 
-bool go(int x) { // 블루레이의 크기가 x일때, 가능한지 확인
-   // 예외처리: 어떤 강의든 블루레이 크기보다 클 수 없음
+int go(int x) { // 블루레이의 크기가 x일때, 가능?
+
+   // 예외처리
+   // 블루레이보다 큰게 담길수없음(?)
+   // 길이가 가장 큰 기타강의 또한 무조건 블루레이에 들어가야 하기 때문에
    for(int i = 0; i < n; i++){
       if(a[i] > x){
          return false;
       }
    }
 
-   // 블루레이 크기가 x일 때 필요한 블루레이 개수 계산
-   int temp = x; // 현재 블루레이의 남은 공간
-   int cnt = 1;  // 블루레이 개수 (처음부터 1개로 시작)
-   
+   // a[i] <= x 임이 보장됨.
+
+   // x : 빼고 남은크기
+   int temp = x; // 블루레이의 크기 저장
+   int cnt = 0;
    for(int i = 0; i < n; i++){
-      if(temp - a[i] < 0){ // 현재 블루레이에 더 담을 수 없을 때
-         temp = x;         // 새 블루레이 사용
-         cnt++;            // 블루레이 개수 증가
+      if(x - a[i] < 0){ // 블루레이가 꽉 찰때마다 카운팅
+         x = temp; // 남은크기 초기화
+         cnt++; // 1칸사용
       }
-      temp -= a[i];        // 현재 블루레이에 강의 추가
+      x -= a[i];
    }
+   if(x != temp) cnt++; // 꽉 차지 않더라도 일부 강의가 기록된 경우
    
-   return cnt <= m;  // m개 이하의 블루레이로 가능한지 반환
+   return cnt <= m;
 }
 
 int main(){
    ios_base::sync_with_stdio(0);
    cin.tie(0); cout.tie(0);
 
-   cin >> n >> m;
-   
-   int sum = 0;
-   int max_lecture = 0;
-   
-   for(int i = 0; i < n; i++) {
-      cin >> a[i];
-      sum += a[i];
-      max_lecture = max(max_lecture, a[i]);
+   cin>>n>>m;
+   for(int i=0;i<n;++i) {
+      cin>>a[i];
    }
 
-   int st = 1;  // 최소 크기는 가장 큰 강의 길이
-   int en = sum;          // 최대 크기는 모든 강의 길이의 합
-   int ret = 0;
-   
-   while(st <= en) {
-      int mid = (st + en) / 2;
-      
+   int st=1;
+   int en=100000*10000;
+   while(st<=en) {
+      int mid = (st+en)/2;
+      // cout<<st<<" "<<en<<" \n";
       if(go(mid)) {
-         ret = mid;       // 가능한 경우 결과 저장
-         en = mid - 1;    // 더 작은 크기 시도
-      } else {
-         st = mid + 1;    // 더 큰 크기 필요
+         en=mid-1;
+         ret=mid;
+      }
+      else {
+         st=mid+1;
       }
    }
-   
-   cout << ret;           // 가능한 최소 크기 출력
+   cout<<st;
 }
