4d308c3c2d5a19a3060258207e2554edc426fc99
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-03-07 22:32:42 +0900

    [Silver I] Title: 용돈 관리, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int n,m,ret;
+int a[100000+4];
+
+int go(int x) { // 인출할돈이 X 일때, 가능한지
+   //인출할돈보다 써야할돈이 크면 반드시 실패
+   for(int i=0;i<n;++i) {
+      if(a[i] > x) {
+         return 0;
+      }
+   }
+   
+
+   int cnt=1; //인출한 횟수
+   int remain=x; // 남은돈
+   for(int i=0;i<n;++i) {
+      if(remain < a[i]) {
+         //남은돈은 무조건 통장에 넣어야함 (통장은 무한?)
+         remain=0;
+         cnt++;
+         remain +=x;
+      }
+      remain -= a[i];
+   }
+   // 마지막 돈도 쓸수있는지 봐야함
+   return cnt <= m ;
+}
+
+int main(){
+   ios_base::sync_with_stdio(0);
+   cin.tie(0); cout.tie(0);
+
+   cin>>n>>m;
+   for(int i=0;i<n;++i) {
+      cin>>a[i];
+   }
+
+   int st=0;
+   int en=100000*10000; // 주의!
+   while(st<=en) {
+      int mid = (st+en)/2;
+      // cout<<st<<" "<<mid<<" " <<en<<" \n";
+      if(go(mid)) {
+         en=mid-1;
+         ret=mid;
+      }
+      else {
+         st=mid+1;
+      }
+   }
+   cout<<ret;
+}
