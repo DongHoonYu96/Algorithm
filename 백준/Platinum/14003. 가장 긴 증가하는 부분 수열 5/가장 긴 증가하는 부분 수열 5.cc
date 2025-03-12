1d7145574b5f0c87c96e59050ed598cf340f4b1d
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-03-12 13:34:03 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 224 ms, Memory: 29740 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,52 +1,43 @@
-#include <bits/stdc++.h>
-#define endl "\n";
+#include<bits/stdc++.h>
+using namespace std; 
 
-using namespace std;
 typedef long long ll;
 
+int len, n;
+ll a[1000000+4];
+ll lis[1000000+4];
+pair<int,int> ans[1000000+4]; // pos,num
 
-ll n, lis[1000000 + 4], len, num;
-ll Index_Arr[1000000 + 4]; //index[3]=1 : 원본3번, lis 1번
-ll a[1000000 + 4]; //원본배열
+int main(){
+   ios_base::sync_with_stdio(0);
+   cin.tie(0); cout.tie(0);
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+   fill(a,a+1000000+4,-1000000000-1);
+   cin>>n;
+   for(int i=0;i<n;++i) {
+      cin>>a[i];
+   }
 
-    //memset(Index_Arr, -1, sizeof(Index_Arr));
+   for(int i=0;i<n;++i) {
+      int idx = lower_bound(lis,lis+len,a[i])-lis; // a가 아니라 lis가 탐색대상!
+      if(idx==len) {
+         len++;
+      }
+      lis[idx]=a[i];
+      ans[i].first = idx;
+      ans[i].second = a[i];
+   }
+   cout<<len<<"\n";
 
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> num;
-        a[i] = num;
-
-        int idx = lower_bound(lis, lis + len, num) - lis;
-  
-        if (idx == len) len++; 
-        lis[idx] = num;
-
-        //lis 배열 찾기위한 로직 추가
-        Index_Arr[i] = idx;
-        
-    }
-    cout << len<<"\n";
-
-    /*for (int i = 0; i < n; ++i) {
-        cout << Index_Arr[i] << " ";
-    }cout << endl;*/
-
-    vector<ll> v;
-    int find_idx = len - 1;
-    for (int i = n-1 ; i >= 0; --i) { //--i 역순탐색 => 3 2 4 에서 2가 자동 pass됨!
-        if (Index_Arr[i] == find_idx) {
-            find_idx--;
-            v.push_back(a[i]);
-        }
-    }
-    reverse(v.begin(), v.end());
-    for (auto i : v) {
-        cout << i << " ";
-    }   
-    return 0;
+   stack<int> s;
+   for(int i=n-1;i>=0;--i) {
+      if(ans[i].first == len-1) {
+         s.push(ans[i].second);
+         len--;
+      }
+   }
+   while(s.size()) {
+      cout<<s.top()<<" ";
+      s.pop();
+   }
 }
-
