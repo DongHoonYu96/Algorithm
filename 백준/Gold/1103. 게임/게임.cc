0ed2509b1a1ab8c841e9dc93a9e776b251991148
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-03-15 16:44:27 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -1,62 +1,92 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h>
+using namespace std; 
 
 typedef long long ll;
 
-ll n,m, a[54][54], dp[54][54], vis[54][54];
+int n,m;
+int a[54][54],vis[54][54];
+int dp[54][54]; // ->, 세로, 대각
+int ret;
 
-//좌표 : 그상태일때, 움직인 최대 횟수
-ll dfs(int y, int x) {
-    if (y < 0 || x < 0 || y >= n || x >= m) return 0;
-    if (a[y][x] == -1) return 0;
-
-    //사이클검사방법
-    //방문한곳을 재방문했다면 사이클임!
-    if (vis[y][x]) {
-       cout << -1;
-       exit(0);
+int dfs(int y, int x) {
+   if(y<0 || x<0 || y>=n || x>=m) {
+      // ret=max(ret,cnt);
+      return 0;
    }
-    if (dp[y][x] != -1) return dp[y][x];
-    //if (dp[y][x][cnt] >= 1e9) {
-    //    cout << -1;
-    //    exit(0);
-    //}
-
-    //방문걸고
-    vis[y][x] = 1;
-    //cout << y << " " << x << " \n";
-    dp[y][x] = 0;
-    ll a1 = dfs(y + a[y][x], x) + 1;
-    ll b = dfs(y , x+ a[y][x] ) + 1;
-    ll c = dfs(y - a[y][x], x)  + 1;
-    ll d = dfs(y, x- a[y][x] ) + 1;
-    vis[y][x] = 0; //방문풀기 (원복해야 다른 결과에 영향을 안미침)
-
-    vector<int> v;
-    v.push_back(a1); v.push_back(b); v.push_back(c); v.push_back(d);
-    dp[y][x] += *max_element(v.begin(), v.end());
-    return dp[y][x];
+   if(a[y][x]==-1) {
+      // ret=max(ret,cnt);
+      return 0;
+   }
+   if(vis[y][x]) {
+      cout<<-1;
+      exit(0);
+   }
+   // cout<<y<<" "<<x<<"\n";
+   
+   int & ret = dp[y][x];
+   if(ret!=-1) {
+      return ret;
+   }
+   ret=0;
+   
+   int num=a[y][x];
+   vis[y][x]=1;
+   ret=max(ret,dfs(y-num,x)+1);
+   ret=max(ret,dfs(y+num,x)+1);
+   ret=max(ret,dfs(y,x+num)+1);
+   ret=max(ret,dfs(y,x-num)+1);
+   vis[y][x]=0;
+   return ret;
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n>>m;
+void dfs2(int y, int x, int cnt) {
+   
+   if(y<0 || x<0 || y>=n || x>=m) {
+      ret=max(ret,cnt);
+      
+      return;
+   }
+   if(a[y][x]==-1) {
+      ret=max(ret,cnt);
+      return;
+   }
+   cout<<y<<" "<<x<<" "<<cnt<<"\n";
+   
 
-    memset(dp, -1, sizeof(dp));
+   int num=a[y][x];
+   dfs2(y-num,x, cnt+1);
+   dfs2(y+num,x, cnt+1);
+   dfs2(y,x+num, cnt+1);
+   dfs2(y,x-num, cnt+1);
+   return;
+}
 
-    for (int i = 0; i < n; ++i) {
-        string s;
-        cin >> s;
-        for (int j = 0; j < m; ++j) {
-            if (s[j] == 'H') a[i][j] = -1;
-            else a[i][j] = s[j] - '0';
-        }
-    }
+int main(){
+   ios_base::sync_with_stdio(0);
+   cin.tie(0); cout.tie(0);
 
-    ll ret= dfs(0, 0);
+   cin>>n>>m;
+   for(int i=0;i<n;++i) {
+      string tmp;
+      cin>>tmp;
+      for(int j=0;j<m;++j) {
+         if(tmp[j]=='H') {
+            a[i][j]=-1; //구멍
+            continue;
+         }
+         a[i][j]=tmp[j]-'0';
+      }
+   }
+   // for(int i=0;i<n;++i) {
+   //    for(int j=0;j<m;++j) {
+   //       cout<<a[i][j]<<" ";
+   //    }cout<<"\n";
+   // }
+   memset(dp,-1,sizeof(dp));
 
-
-    cout << ret;
-    return 0;
+   cout<<dfs(0,0);
+   // dfs2(0,0,0);
+   // cout<<ret;
+   // cout<<dfs(0,0,0);
+   // cout<<ret;
 }
