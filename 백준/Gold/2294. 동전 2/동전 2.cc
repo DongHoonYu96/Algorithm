0d2a479cceadee7747fa47e548f369970b2b6ae3
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-03-17 14:03:58 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,26 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h>
+using namespace std; 
 
-int n, k,coin;
-int dp[10000 + 4]; //dp[i] : i원을 만들수있는 경우의수
+typedef long long ll;
 
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+int n,k;
+ll dp[10000+4];
 
-	cin >> n >> k;
-
-	fill(dp, dp + 10000 + 4, 10004); //최소값구해야함 -> 최대값으로 초기화
-
-	dp[0] = 0;
-	for (int i = 1; i <= n; ++i) { //동전만큼 회차
-		cin >> coin;
-		for (int j = coin; j <= k; ++j) {
-			dp[j] = min(dp[j], dp[j - coin] + 1);
-		}
-	}
-	if (dp[k] == 10004) cout << -1;
-	else cout << dp[k];
-
-	return 0;
+int main(){
+   ios_base::sync_with_stdio(0);
+   cin.tie(0); cout.tie(0);
+   
+   cin>>n>>k;
+      
+   fill(dp,dp+10000+4,987654321);
+   dp[0]=0;
+   
+   for(int i=0;i<n;++i) {
+      int tmp;
+      cin>>tmp; //2원
+      for(int j=tmp;j<=k;++j) {
+         dp[j]=min(dp[j],dp[j-tmp]+1);
+      }
+   }
+   if(dp[k]==987654321) {
+      cout<<-1;
+      exit(0);
+   }
+   cout<<dp[k];
+   
 }
