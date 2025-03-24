6bbd64721408e0768889bf03f1d9c2f6ccd72f01
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-03-24 19:47:55 +0900

    [Gold IV] Title: 좋다, Time: 100 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -1,55 +1,36 @@
+// Authored by : scsc3204
+// Co-authored by : -
+// http://boj.kr/cf7ce676aad54a2786941423afd99612
 #include <bits/stdc++.h>
-
 using namespace std;
-typedef long long ll;
 
-ll ret;
-int n,vis[2004];
-vector<ll> v;
+int cnt, n;
+vector<int> a;
+
+void solve(int i) {
+  for(int j = 0; j < n; j++) {
+    if(j == i) continue;
+    int x = a[i] - a[j];
+    int idx = lower_bound(a.begin(), a.end(), x) - a.begin();
+    while(idx < n && a[idx] == x) {
+      if(idx != i && idx != j) { cnt++; return; }
+      idx++;
+    }
+  }
+}
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n;
-	for(int i=0;i<n;++i){
-		ll tmp;
-		cin >> tmp;
-		v.push_back(tmp);
-	}
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-	sort(v.begin(), v.end());
-	//for (auto i : v) cout << i << " ";
-	//cout << "\n";
+  cin >> n;
+  for(int i = 0; i < n; i++) {
+    int x; cin >> x;
+    a.push_back(x);
+  }
 
-	for (int i = 0; i < n-1; ++i) {
-		for (int j = i + 1; j < n; ++j) {
-			ll cur = v[i]+v[j];
-			//cout << i << " " << j << "\n";
-			int LB = lower_bound(v.begin() , v.end(), cur) - v.begin();
-			int UB = upper_bound(v.begin(), v.end(), cur) - v.begin();
+  sort(a.begin(), a.end());
 
-			//해당수가 존재하는경우
-			if (UB - LB) {
-				//cout << i << " " << j <<" " << v[LB] << "\n";
-				if (vis[LB]) continue; //해당수는 이미 좋은수인경우 패스
-				if (i == LB || j == LB) continue;
-				ret += (UB - LB);
-				vis[LB] = 1;
-			}
-			//ret += (UB - LB);
-			//cout << i << " " << j << "\n";
-			//int idx = lower_bound(v.begin() + i + 1, v.end(), cur) - v.begin();
-			//for (int k = -1; k <= 1; ++k) {
-			//	if (idx + k < 0 || idx + k >= n) continue; //범위쳌
-			//	if (idx + k == i) continue; //자기자신제외
-			//	if (abs(ret) > abs(cur + v[idx + k])) { //정답갱신
-			//		ret++;
-			//	}
-			//}
-		}
-		
-	}
-	
-	cout << ret;
-	return 0;
+  for(int i = 0; i < n; i++) solve(i);
+  cout << cnt;
 }
