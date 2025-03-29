649eb59a3c1b370d8f58a3bfed9d9bab0d045467
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-03-29 22:14:23 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,46 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+using namespace std; 
 
-using namespace std;
-using ll = long long;
+typedef long long ll;
 
-ll n, m, ret;
-vector<int> arr;
-
-void combi(int idx, vector<int>& v) {
-    if(v.size()==2) {
-        int a = arr[v[0]];
-        int b = arr[v[1]];
-        if(a+b == m) {
-            ret++;
-        }
-        return;
-    }
-    for(int i=idx+1 ; i<n;++i) {
-        v.push_back(i);
-        combi(i,v);
-        v.pop_back();
-    }
-}
+ll n,m,ret;
+vector<int> v;
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> n >> m;
+   ios_base::sync_with_stdio(0);
+   cin.tie(0); cout.tie(0);
 
-    if(m > 200000) {
-        cout<<0;
-        return 0;
-    }
-    
-    for (int i = 0; i < n; ++i) {
-        int tmp;
-        cin>>tmp;
-        arr.push_back(tmp);
-    }
-    
-    vector<int> v;
-    combi(-1,v);
-    
-    cout<<ret;
+   cin>>n>>m;
+   for(int i=0;i<n;++i) {
+      int tmp;
+      cin>>tmp;
+      v.push_back(tmp);
+   }
+   sort(v.begin(),v.end());
+   
+   for(int i=0;i<n;++i) {
+      if(binary_search(v.begin()+i+1,v.end(),abs(m-v[i]))) {
+         ret++;
+      }
+   }
+   cout<<ret;
+   
+   return 0;
 }
