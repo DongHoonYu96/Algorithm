e06307519f7c15da0fd9370c9cf5e526d28ec763
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-04-06 16:22:06 +0900

    [Gold V] Title: 1학년, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.cc
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -42,13 +42,9 @@ int main() {
     for(int i=0;i<n;++i) {
         cin>>arr[i];
     }
-
-    if(arr[0]<0) {
-        cout<<0;
-        return 0;
-    }
     
     // cout<<dfs(0,0);
+    // 안되는 이유 : 첫숫자를 2번사용하게됨.?
 
     cout << dfs(1, arr[0]);
     
