ab78fa5421289f87baeb70473f045db7ad7e010f
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-05-02 17:09:00 +0900

    [level 3] Title: 불량 사용자, Time: 41.37 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
 
 ### 성능 요약
 
-메모리: 127 MB, 시간: 96.20 ms
+메모리: 77.6 MB, 시간: 41.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 02일 16:37:13
+2025년 05월 02일 17:08:59
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -3,11 +3,10 @@ class Solution {
     static int answer;
     static int n,m;
     static String[] uid,bid;
-    static boolean[] vis = new boolean[9];
-    static Set<String> resultSet = new HashSet<>(); // 중복 제거를 위한 Set 추가
+    static Set<Integer> resultSet = new HashSet<>(); // 중복 제거를 위한 Set 추가
     
     //a와 b가 매칭되는지
-    static boolean go2(String a, String b){
+    static boolean check(String a, String b){
         if(a.length()!=b.length()) return false;
         
         for(int i=0;i<a.length();++i){
@@ -21,65 +20,20 @@ class Solution {
         return true;
     }
     
-    static void perm(ArrayList<Integer> v, boolean[] visited) {
-        // 기저 조건: m개를 모두 뽑았으면 결과 사용
-        if (v.size() == n) {
-            // 예: 결과 출력 또는 go(v) 호출
-            // System.out.println(v);
-            dfs(v);
-            return;
-        }
-
-        // 0부터 n-1까지 모든 후보 인덱스 시도
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i]) {             // 아직 사용되지 않은 인덱스만
-                visited[i] = true;         // i 선택
-                v.add(i);
-
-                perm(v, visited);       // 다음 자리 재귀
-
-                v.remove(v.size() - 1);    // 백트래킹
-                visited[i] = false;        // i 선택 해제
-            }
-        }
-    }
-    
-    //v : nPn 순열 인덱스
-    static void dfs(ArrayList<Integer> v){
-        ArrayList<String> tmp = new ArrayList<>();
-        for(int i=0;i<m;++i){
-            String u = uid[v.get(i)];
-            if(go2(u,bid[i])){
-                tmp.add(u);
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        if(tmp.size()==bid.length){
-            Collections.sort(tmp);
-            for(String s : tmp){
-                sb.append(s);
-            }
-            resultSet.add(sb.toString());
-        }
-    }
-    
-    static void combi(int idx, ArrayList<Integer> v){
-        if(v.size()==m){
-            // for(int i : v){
-            //     System.out.print(i+" ");
-            // }
-            // System.out.println();
-            dfs(v);
+    void dfs(int i, int bit){
+        if(i==m){
+            resultSet.add(bit);
             return;
         }
         
-        for(int i=idx+1;i<n;++i){
-            v.add(i);
-            combi(i,v);
-            v.remove(v.size()-1);
+        for(int j=0;j<n;++j){
+            if((bit & (1<<j)) > 0) continue;
+            if(!check(uid[j],bid[i])) continue;
+            dfs(i+1, bit | (1<<j));
         }
     }
+    
+
     
     public int solution(String[] user_id, String[] banned_id) {
         n=user_id.length;
@@ -90,7 +44,7 @@ class Solution {
         // ArrayList<Integer> v = new ArrayList<>();
         // combi(-1,v);
         
-        perm(new ArrayList<Integer>(), new boolean[9]);
+        dfs(0,0);
         answer=resultSet.size();
         return answer;
     }
