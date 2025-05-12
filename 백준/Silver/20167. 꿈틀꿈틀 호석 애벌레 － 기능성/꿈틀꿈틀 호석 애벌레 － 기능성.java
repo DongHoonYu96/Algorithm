c04aeb5d38f76329f8b3af8a046bb859d8915c0c
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-05-12 13:13:52 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 176 ms, Memory: 18524 KB, Score: 31 point -BaekjoonHub

---
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static long ret, n, k;
+    static long[] dp = new long[100000+4];
+    static ArrayList<Long> arr = new ArrayList<>();
+
+    // 축척된에너지, 얻은탈피에너지
+    static void dfs(int idx, long acc, long tal) {
+        if(acc >=k){
+            tal += acc-k;
+            acc=0;
+        }
+
+        if(idx==n){
+            ret=Math.max(ret,tal);
+            return;
+        }
+
+        dfs(idx+1,acc + arr.get(idx), tal); //선택
+        dfs(idx+1,acc, tal); //선택
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        Arrays.fill(dp,-1);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        for(int i=0;i<n;++i){
+            int tmp = sc.nextInt();
+            arr.add(Long.valueOf(tmp));
+        }
+        dfs(0,0,0);
+        System.out.println(ret);
+    }
+}
