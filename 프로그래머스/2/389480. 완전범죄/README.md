5a356d7c64749c2c6839df33d7c698f0b9a07df4
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-05-13 03:03:50 +0900

    [level 2] Title: 완전범죄, Time: 17.79 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/README.md
+++ 프로그래머스/2/389480. 완전범죄/README.md
@@ -1,10 +1,10 @@
 # [level 2] 완전범죄 - 389480 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389480) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389480?language=java) 
 
 ### 성능 요약
 
-메모리: 78.7 MB, 시간: 3.39 ms
+메모리: 77.8 MB, 시간: 17.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 13일 02:36:05
+2025년 05월 13일 03:03:48
 
 ### 문제 설명
 
--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,44 +1,41 @@
 import java.util.*;
-
 class Solution {
-    static int n, m;
-    static Integer[][][] dp;
-    
-    int dfs(int idx, int a, int b, int[][] info) {
-        // 기저 사례: 모든 요소를 처리한 경우
-        if (idx == info.length) {
-            return (a <= 0 || b <= 0) ? Integer.MAX_VALUE : Math.abs(n - a);
+    public static int len;
+    public static int answer=Integer.MAX_VALUE;
+    public static Set<String> visited = new HashSet<>();
+    public int solution(int[][] info, int n, int m) {
+        // Arrays.sort(info, (a, b) -> (b[0] + b[1]) - (a[0] + a[1]));
+        // for(int [] i: info){
+        //     System.out.println(i[0]+" "+i[1]);
+        // }
+        len=info.length;
+        dfs(info,n,m,0,0,0);
+        if(answer==Integer.MAX_VALUE){
+            return -1;
         }
-        
-        // 유효하지 않은 상태
-        if (a <= 0 || b <= 0) {
-            return Integer.MAX_VALUE;
-        }
-        
-        // 이미 계산된 값이 있으면 반환
-        if (dp[idx][a][b] != null) {
-            return dp[idx][a][b];
-        }
-        
-        // 현재 요소를 처리하는 두 가지 방법 시도
-        int result = Math.min(
-            dfs(idx + 1, a - info[idx][0], b, info),
-            dfs(idx + 1, a, b - info[idx][1], info)
-        );
-        
-        // 결과 저장 및 반환
-        return dp[idx][a][b] = result;
+        return answer;
+        //dfs인데 이미 기존의 최솟값보다 커진 경우 BREAK
+        //만약 이미 sumA가 크거나 같다면 
+
     }
-    
-    public int solution(int[][] info, int _n, int _m) {
-        n = _n;
-        m = _m;
-        
-        // 필요한 크기만큼만 DP 배열 초기화 (null로 초기화됨)
-        dp = new Integer[info.length + 1][n + 1][m + 1];
-        
-        int result = dfs(0, n, m, info);
-        
-        return (result == Integer.MAX_VALUE) ? -1 : result;
+    public void dfs(int [][] info, int n, int m, int sumA, int sumB, int depth){
+        if(sumA>=n || sumB >=m || sumA >=answer){
+            return;
+        }
+        if(depth==len){
+            //calc
+            answer=Math.min(answer,sumA);
+            return;
+        }
+
+        String state = sumA + "," + sumB + "," + depth;
+        if (visited.contains(state)) return;
+
+        visited.add(state);
+        //a가 포함하는 경우
+        dfs(info,n,m, sumA+info[depth][0], sumB, depth+1);
+        //b가 포함하는 경우
+        dfs(info,n,m, sumA, sumB+info[depth][1], depth+1);
+
     }
 }
