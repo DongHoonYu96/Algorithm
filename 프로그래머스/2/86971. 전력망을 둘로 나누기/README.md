f733b80397ea210f073cab6ed247624a669ff1c0
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-08-20 21:38:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.32 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 88.4 MB, 시간: 10.45 ms
+메모리: 83.4 MB, 시간: 0.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 20일 17:32:51
+2025년 08월 20일 21:38:00
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -5,24 +5,25 @@ class Solution {
     static int[][] graph = new int[104][104];
     static boolean[] vis = new boolean[104];
     static int n;
+    static int answer = Integer.MAX_VALUE;
     
     int dfs(int node){
         vis[node]=true;
         
-        int ret=1;
-        for(int i=1;i<=n;++i){
-            if(vis[i]) continue;
-            
-            if(graph[node][i]==1){
-                ret+=dfs(i);
-            }
-        }
-        return ret;
+        int child=1; // 자기자신 카운팅
         
+        for(var nxt : adj[node]){
+            if(vis[nxt]) continue;
+            
+            child+=dfs(nxt);
+        }
+        
+        answer = Math.min(answer,Math.abs((n-child)-child));
+        return child;
     }
     
     public int solution(int N, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
+        
         n=N;
         for(int i=0;i<104;++i){
             adj[i]=new ArrayList<>();
@@ -32,38 +33,10 @@ class Solution {
             int to  = wire[1];
             adj[from].add(to);
             adj[to].add(from);
-            graph[from][to]=1;
-            graph[to][from]=1;
         }
-        // for(int i=1;i<n;++i){
-        //     System.out.println(adj[i]);
-        // }
+
+        dfs(1);
         
-        // 연결끊을 index 고르기
-        // 연결끊기
-        // dfs
-        // 원상복구
-        for(int i=0;i<wires.length;++i){
-            int from = wires[i][0];
-            int to = wires[i][1];
-            
-            graph[from][to]=0;
-            graph[to][from]=0;
-            
-            Arrays.fill(vis,false);
-            var v = new ArrayList<Integer>();
-            for(int j=1;j<=n;++j){
-                if(vis[j]) continue;
-                
-                v.add(dfs(j));
-            }
-            
-            graph[from][to]=1;
-            graph[to][from]=1;
-            
-            // System.out.println(v.get(0) +" " + v.get(1));
-            answer = Math.min(answer,Math.abs(v.get(0)-v.get(1)));
-        }
         return answer;
     }
 }
